026c9ffc7dc70c1911a9af5cb499c09b4c91cb10
Benjamin Nachman <bnachman@cern.ch>
2017-06-09 11:38:34 +0200

    LHE reader and analyzer

---
+++ fixedorder/myread.C
@@ -0,0 +1,12 @@
+#include "LHEF.h"
+#include <cstdlib>
+#include <string>
+#include <iostream>
+
+void mymain(){
+
+	// Init readers and writers
+  	LHEF::Reader* reader = 0;
+	reader = new LHEF::Reader("unweighted_events.lhe");
+
+}
